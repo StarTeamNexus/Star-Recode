b62104033e22002258cf1d553a91ef2fa3d6b293
Scaro <123261052+RipAngelhacks@users.noreply.github.com>
2025-08-06 03:02:45 -0400

    Create WebsiteClick.js

---
+++ Javascript/WebsiteClick.js
@@ -0,0 +1,7 @@
+// Credits To Soul, I guess this is simple, but I am still gonna give credit
+function navToGames() {
+  window.location.href = "https://example.com/" //this will change when a game.html file is made
+}
+function navToProxy() {
+  window.location.href = "https://example.com/" //this will change when a proxy.html file is made
+}
