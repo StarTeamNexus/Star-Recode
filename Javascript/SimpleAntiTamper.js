28d48a6102a6642abde34d86f640b54ebd798eb6
Scaro <123261052+RipAngelhacks@users.noreply.github.com>
2025-08-11 22:16:32 -0400

    Update SimpleAntiTamper.js

--- Javascript/SimpleAntiTamper.js
+++ Javascript/SimpleAntiTamper.js
@@ -12,22 +12,23 @@
       if (e.key === 'F12') {
         e.preventDefault();
         alert("You have been detected for attempting to Edit or Tamper Star's code")
+        window.location.href = "TamperDetected.html" //this will change when a game.html file is made
       }
       // Prevent Ctrl+Shift+I (Inspect Element)
       if (e.ctrlKey && e.shiftKey && e.key === 'I') {
         e.preventDefault();
-                alert("You have been detected for attempting to Edit or Tamper Star's code")
-
+        alert("You have been detected for attempting to Edit or Tamper Star's code")
+        window.location.href = "TamperDetected.html" //this will change when a game.html file is made
       }
       if (e.ctrlKey && e.shiftKey && e.key === 'K') {
         e.preventDefault();
-                alert("You have been detected for attempting to Edit or Tamper Star's code")
-
+        alert("You have been detected for attempting to Edit or Tamper Star's code")
+        window.location.href = "TamperDetected.html" //this will change when a game.html file is made
       }
       // Prevent Ctrl+U (View Source)
       if (e.ctrlKey && e.key === 'u') {
         e.preventDefault();
-                alert("You have been detected for attempting to Edit or Tamper Star's code")
-
+        alert("You have been detected for attempting to Edit or Tamper Star's code")
+        window.location.href = "TamperDetected.html" //this will change when a game.html file is made
       }
     });
