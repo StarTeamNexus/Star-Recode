450fe104bf75d0136162b9f083f43314f10b74ad
Scaro <123261052+RipAngelhacks@users.noreply.github.com>
2025-08-25 11:03:53 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     cdnBase: 'https://starteamnexus.github.io/files/',
-    listFile: 'https://raw.githubusercontent.com/StarTeamNexus/files/main/list.json',
+    listFile: 'https://raw.githubusercontent.com/StarTeamNexus/files/refs/heads/main/list.json',
     defaultIcon: './assets/images/default-icon.png',
     owner: 'StarTeamNexus',
     repo: 'files'
